b99083996e07f41ab98ccf44cd2f6cab7aab474f
Sakthisks <sakthikarthigaiselvi@gmail.com>
2020-11-24 19:49:02 -0500

    Updates

---
+++ precache-manifest.221a88373f54740ffc4ff51c1cf8576a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f6d6273b203fed9b7c1",
+    "url": "/me/static/css/main.dc0489ed.chunk.css"
+  },
+  {
+    "revision": "6f6d6273b203fed9b7c1",
+    "url": "/me/static/js/main.6f6d6273.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "12fd2fe0be2446f516c8",
+    "url": "/me/static/css/2.cae61f01.chunk.css"
+  },
+  {
+    "revision": "12fd2fe0be2446f516c8",
+    "url": "/me/static/js/2.12fd2fe0.chunk.js"
+  },
+  {
+    "revision": "0e3da02ba36762652d4dd8c86b2b1d39",
+    "url": "/me/index.html"
+  }
+];
